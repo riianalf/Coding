bd894471a0b469dd4ca0d1ad369b4e6d5aa34c8e
Klinik Dokter Edi <klinikdokteredi@gmail.com>
2025-09-04 15:16:48 +0700

    Edit Pasien

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
+
+    {{ $slot }}
+
 </x-layouts.app.sidebar>
--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -1,10 +1,3 @@
 <x-layouts.app :title="'Daftar Pasien'">
-   <div class="p-4">
-      <div class="flex justify-between items-center mb-4">
-         <h1 class="text-xl font-semibold">Daftar Pasien</h1>
-         <a href="{{ route('patients.create') }}" class="btn btn-primary">Tambah Pasien</a>
-      </div>
-
-      @include('pasien')
-   </div>
+    @include('pasien')
 </x-layouts.app>
